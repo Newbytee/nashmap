171d563cf4be10ec48d1c96560d5d84720ac54f5
Kir Neboula <newbie13xd@outlook.com>
2018-12-09 22:43:28 +0100

    Did stuff

--- src/Nashmap.java
+++ src/Nashmap.java
@@ -1,9 +1,34 @@
-public class Nashmap {
-    public Nashmap() {
+public class Nashmap<T> {
+    private NashmapElement[] elements;
+    private int currentIndex;
+    private static class NashmapElement<Type> {
+        Type value;
+        int key;
 
+        NashmapElement(int key, Type value) {
+            this.value = value;
+            this.key = key;
+        }
+
+        Type getValue() {
+            return value;
+        }
     }
 
-    public boolean add() {
-        return true;
+    public Nashmap(int size) {
+        this.elements = new NashmapElement<?>[size];
+        this.currentIndex = 0;
+    }
+
+    public void add(String key, Object object) {
+        this.elements[currentIndex] = new NashmapElement<>(key.hashCode(), object);
+        this.currentIndex++;
+    }
+
+    public Object get(String key) {
+        for (int i = 0; i < currentIndex; i++) {
+            elements[i].getValue();
+        }
+        return null;
     }
 }
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Nashmap<Integer> map = new Nashmap<>(10);
+
+
+    }
+}
