032371b6a0095ca7492e8606cf1456d8b48f54b1
Kir Neboula <newbie13xd@outlook.com>
2018-12-11 08:26:29 +0100

    Speed test stuff

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        Nashmap map = new Nashmap(2);
+        long test = System.nanoTime();
+        Nashmap map = new Nashmap(1000000);
 
         for (int i = 0; i < 1000000; i++) {
             map.add("foobar" + Integer.toString(i), "meep");
@@ -13,7 +14,7 @@ public class Test {
 
         //System.out.println(map.get("foobar"));
         //System.out.println(map.size());
-        map.remove("foobar");
+        //map.remove("foobar");
         //System.out.println(map.get("shrek"));
         //map.resize(0);
         System.out.println(map.elements());
@@ -22,5 +23,6 @@ public class Test {
         //System.out.println(map.get("succ"));
 
         //System.out.println("foobar".hashCode() % 100);
+        System.out.println(System.nanoTime() - test);
     }
 }
